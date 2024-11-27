f8bb2cb58238dd9cae0935d42a966d5438668fd8
jjribeiro1 <jvsr.99@hotmail.com>
2024-11-27 14:14:40 -0300

    register form schema and styles

---
+++ src/features/auth/components/register-form.tsx
@@ -0,0 +1,112 @@
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+  CreateUserInput,
+  createUserSchema,
+} from "../schemas/create-user-schema";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+export function RegisterForm() {
+  const form = useForm<CreateUserInput>({
+    resolver: zodResolver(createUserSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
+  function onSubmit(data: CreateUserInput) {
+    console.log(data);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <Card className="w-96">
+          <CardHeader>
+            <CardTitle className="text-center">Crie sua conta</CardTitle>
+          </CardHeader>
+          <CardContent className="flex flex-col gap-4">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Nome</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Senha</FormLabel>
+                  <FormControl>
+                    <Input type="password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="confirmPassword"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Confirme sua senha</FormLabel>
+                  <FormControl>
+                    <Input type="password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </CardContent>
+          <CardFooter>
+            <Button className="w-full" type="submit">Registrar</Button>
+          </CardFooter>
+        </Card>
+      </form>
+    </Form>
+  );
+}
---
+++ src/features/auth/schemas/create-user-schema.ts
@@ -0,0 +1,25 @@
+"use client";
+import { z } from "zod";
+
+export const createUserSchema = z
+  .object({
+    name: z
+      .string()
+      .min(3, { message: "nome deve ter no mínimo 3 caracteres" })
+      .max(200, { message: "nome deve ter no máximo 200 caracteres" }),
+    email: z
+      .string()
+      .email({ message: "email inválido" })
+      .max(200, { message: "email deve ter no máximo 200 caracteres" }),
+    password: z
+      .string()
+      .min(6, { message: "senha deve ter no mínimo 6 caracteres" })
+      .max(200, { message: "senha deve ter no máximo 200 caracteres" }),
+    confirmPassword: z.string(),
+  })
+  .refine((ctx) => ctx.password === ctx.confirmPassword, {
+    message: "confirmação de senha inválida",
+    path: ["confirmPassword"],
+  });
+
+export type CreateUserInput = z.infer<typeof createUserSchema>;
