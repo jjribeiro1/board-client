6bea104b446a4dc4542bed81abff529f1ac324f9
jjribeiro1 <jvsr.99@hotmail.com>
2024-11-28 16:53:33 -0300

    shadcn toast setup

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { Toaster } from "@/components/ui/toaster";
 
 export default function AppProviders({
   children,
@@ -18,6 +19,7 @@ export default function AppProviders({
         disableTransitionOnChange
       >
         {children}
+        <Toaster />
       </NextThemesProvider>
     </QueryClientProvider>
   );
