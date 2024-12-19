709d255775019a4e5a4c1c8cec189dc17a78b694
jjribeiro1 <jvsr.99@hotmail.com>
2024-12-19 20:19:06 -0300

    redirect to organization selection if no orgId cookie is present

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
 
-export default function HomePage() {
-  return (
-    <div>Home</div>
-  )
+export default async function HomePage() {
+  const cookieStore = await cookies();
+  const selectedOrgId = cookieStore.get("orgId")?.value;
+  if (!selectedOrgId) {
+    redirect("/select-org");
+  }
+
+  return <div>Home</div>;
 }
