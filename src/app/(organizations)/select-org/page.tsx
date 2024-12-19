8f11362e3bed29d87148068a42dcb0bb04dda612
jjribeiro1 <jvsr.99@hotmail.com>
2024-12-19 20:59:31 -0300

    add link to create-org page

--- src/app/(organizations)/select-org/page.tsx
+++ src/app/(organizations)/select-org/page.tsx
@@ -1,11 +1,18 @@
+import Link from "next/link";
 import { OrganizationSelect } from "@/features/organizations/components/organization-select";
+import { buttonVariants } from "@/components/ui/button";
 
 export default function SelectOrganizationPage() {
   return (
     <main className="h-screen flex flex-col items-center justify-center">
       <div className="flex flex-col gap-8 w-96">
-        <h1 className="text-2xl text-center font-medium">Escolha sua organização</h1>
+        <h1 className="text-2xl text-center font-medium">
+          Escolha sua organização
+        </h1>
         <OrganizationSelect />
+        <Link href={"/create-org"} className={buttonVariants({ size: "lg" })}>
+          Criar nova organização
+        </Link>
       </div>
     </main>
   );
