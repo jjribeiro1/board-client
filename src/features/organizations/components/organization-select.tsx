84aa14e5c1fc28fd34c678fc93d5c6f9f9d9e4e6
jjribeiro1 <jvsr.99@hotmail.com>
2024-12-19 20:45:19 -0300

    add loading indicator to organization selection component

--- src/features/organizations/components/organization-select.tsx
+++ src/features/organizations/components/organization-select.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { LoaderCircle } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { useLoggedUserInfo } from "@/hooks/use-logged-user-info";
 import { useOrganizationsFromUser } from "../hooks/use-organizations-from-user";
@@ -6,21 +7,27 @@ import { selectOrganizationAction } from "../actions/select-organization-action"
 
 export function OrganizationSelect() {
   const { data: userInfo } = useLoggedUserInfo();
-  const { data: organizations } = useOrganizationsFromUser(userInfo?.id);
+  const { data: organizations, isPending } = useOrganizationsFromUser(userInfo?.id);
 
   return (
     <div className="flex flex-col gap-4">
-      {organizations?.map((organization) => (
-        <Button
-          key={organization.id}
-          onClick={() => selectOrganizationAction(organization.id)}
-          className="w-full"
-          variant={"outline"}
-          size={"lg"}
-        >
-          {organization.name}
-        </Button>
-      ))}
+      {isPending ? (
+        <div className="flex justify-center h-52">
+          <LoaderCircle />
+        </div>
+      ) : (
+        organizations?.map((organization) => (
+          <Button
+            key={organization.id}
+            onClick={() => selectOrganizationAction(organization.id)}
+            className="w-full"
+            variant={"outline"}
+            size={"lg"}
+          >
+            {organization.name}
+          </Button>
+        ))
+      )}
     </div>
   );
 }
