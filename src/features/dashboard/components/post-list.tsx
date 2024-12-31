c693e48c0d43257bb1ca917f116f16e27e26d236
jjribeiro1 <jvsr.99@hotmail.com>
2024-12-31 15:18:15 -0300

    PostStatusDropdown component

--- src/features/dashboard/components/post-list.tsx
+++ src/features/dashboard/components/post-list.tsx
@@ -8,24 +8,16 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
-import { Button } from "@/components/ui/button";
+import { PostStatusDropdown } from "./post-status-dropdown";
 import { usePostsFromOrganization } from "../hooks/use-posts-from-organization";
 
 type Props = {
   orgId: string;
 };
 
-const statusColors: { [key in number]: string } = {
-  1: "bg-primary-foreground text-white hover:bg-primary-foreground/85",
-  2: "bg-purple-500 text-white hover:bg-purple-500/85",
-  3: "bg-yellow-500 text-white hover:bg-yellow-500/85",
-  4: "bg-blue-500 text-white hover:bg-blue-500/85",
-  5: "bg-green-500 text-white hover:bg-green-500/85",
-  6: "bg-red-500 text-white hover:bg-red-500/85",
-};
-
 export function PostList(props: Props) {
   const { data } = usePostsFromOrganization(props.orgId);
+
   return (
     <section className="flex flex-col gap-4">
       {data?.map((post) => (
@@ -33,12 +25,7 @@ export function PostList(props: Props) {
           <CardHeader className="flex flex-row items-center justify-between">
             <CardTitle>{post.title}</CardTitle>
             <CardDescription>
-              <Button
-                size={"sm"}
-                className={`${statusColors[post.status.order]}`}
-              >
-                {post.status.name}
-              </Button>
+              <PostStatusDropdown post={post} />
             </CardDescription>
           </CardHeader>
           <CardFooter className="justify-between">
---
+++ src/features/dashboard/components/post-status-dropdown.tsx
@@ -0,0 +1,77 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { useSystemDefaultStatus } from "@/hooks/use-system-default-status";
+import { useUpdatePostStatusMutation } from "@/features/posts/mutations/use-update-post-status-mutation";
+
+const statusColors: { [key in number]: string } = {
+  1: "bg-primary-foreground text-white hover:bg-primary-foreground/85",
+  2: "bg-purple-500 text-white hover:bg-purple-500/85",
+  3: "bg-yellow-500 text-white hover:bg-yellow-500/85",
+  4: "bg-blue-500 text-white hover:bg-blue-500/85",
+  5: "bg-green-500 text-white hover:bg-green-500/85",
+  6: "bg-red-500 text-white hover:bg-red-500/85",
+};
+
+type Props = {
+  post: {
+    id: string;
+    status: {
+      id: string;
+      name: string;
+      order: number;
+    };
+  };
+};
+
+export function PostStatusDropdown(props: Props) {
+  const { data } = useSystemDefaultStatus();
+  const { mutate } = useUpdatePostStatusMutation(props.post.id);
+
+  function filterStatus(status: {
+    id: string;
+    name: string;
+    order: number | null;
+  }) {
+    if (
+      status.order === null ||
+      status.order <= 1 ||
+      status.name === props.post.status.name
+    ) {
+      return false;
+    }
+
+    return true;
+  }
+
+  function handlePostStatusChange(statusId: string) {
+    mutate({ statusId });
+  }
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          size={"sm"}
+          className={`${statusColors[props.post.status.order]} py-1 h-max`}
+        >
+          {props.post.status.name}
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        {data?.filter(filterStatus).map((status) => (
+          <DropdownMenuItem
+            key={status.id}
+            onClick={() => handlePostStatusChange(status.id)}
+          >
+            {status.name}
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
