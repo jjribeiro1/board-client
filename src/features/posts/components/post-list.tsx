3fc7a887fcb1c05a61a16ed190e63a32b22e589c
jjribeiro1 <jvsr.99@hotmail.com>
2025-01-28 20:20:17 -0300

    add loading and error handling to post list component

--- src/features/posts/components/post-list.tsx
+++ src/features/posts/components/post-list.tsx
@@ -13,13 +13,23 @@ type Props = {
 
 export function PostList(props: Props) {
   const { getQueryParam } = useQueryParams();
-  const { data } = useOrganizationPosts({
+  const { data, isPending, error } = useOrganizationPosts({
     orgId: props.orgId,
     filters: {
       status: getQueryParam("status") as string,
       board: getQueryParam("board") as string,
     },
   });
+
+  if (isPending) {
+    return <div className="font-semibold leading-none tracking-tight">Carregando posts...</div>;
+  }
+
+  if (error) {
+    return <div>Erro ao carregar posts da sua organização</div>;
+  }
+
+  const hasPosts = data.length > 0;
 
   return (
     <section className="flex flex-col gap-6">
@@ -31,35 +41,47 @@ export function PostList(props: Props) {
 
         <CreatePost organizationId={props.orgId} />
       </div>
-      {data?.map((post) => (
-        <Card key={post.id}>
-          <CardHeader className="flex flex-row items-center justify-between">
-            <CardTitle>{post.title}</CardTitle>
-            <CardDescription>
-              <PostStatusDropdown post={post} orgId={props.orgId} />
-            </CardDescription>
-          </CardHeader>
-          <CardFooter className="justify-between">
-            <div className="flex items-center gap-6 w-full">
-              <div className="flex items-center gap-2">
-                <Tag className="w-4 h-4" /> <span>{post.board.title}</span>
+      {hasPosts ? (
+        data.map((post) => (
+          <Card key={post.id}>
+            <CardHeader className="flex flex-row items-center justify-between">
+              <CardTitle>{post.title}</CardTitle>
+              <CardDescription>
+                <PostStatusDropdown post={post} orgId={props.orgId} />
+              </CardDescription>
+            </CardHeader>
+            <CardFooter className="justify-between">
+              <div className="flex items-center gap-6 w-full">
+                <div className="flex items-center gap-2">
+                  <Tag className="w-4 h-4" /> <span>{post.board.title}</span>
+                </div>
+                <div className="flex items-center gap-2">
+                  <Calendar className="w-4 h-4" />
+                  <span>{new Date(post.createdAt).toLocaleDateString()}</span>
+                </div>
               </div>
-              <div className="flex items-center gap-2">
-                <Calendar className="w-4 h-4" />
-                <span>{new Date(post.createdAt).toLocaleDateString()}</span>
-              </div>
-            </div>
 
-            <div className="flex items-center gap-2">
-              {post.tags.map(({ tag }) => (
-                <Badge key={tag.id} variant={"secondary"} className="text-nowrap">
-                  {tag.name}
-                </Badge>
-              ))}
-            </div>
-          </CardFooter>
-        </Card>
-      ))}
+              <div className="flex items-center gap-2">
+                {post.tags.map(({ tag }) => (
+                  <Badge key={tag.id} variant={"secondary"} className="text-nowrap">
+                    {tag.name}
+                  </Badge>
+                ))}
+              </div>
+            </CardFooter>
+          </Card>
+        ))
+      ) : (
+        <div className="flex flex-col items-center justify-center gap-y-8 h-96">
+          <div>
+            <p className="text-2xl font-semibold tracking-tight">Parece que você não tem nenhum post criado</p>
+            <p className="font-medium text-muted-foreground">
+              Aqui você consegue gerenciar todos os Posts da sua organização
+            </p>
+          </div>
+          <CreatePost organizationId={props.orgId} title="Criar primeiro post" />
+        </div>
+      )}
     </section>
   );
 }
