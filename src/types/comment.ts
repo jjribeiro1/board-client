3bf1e0cd9b39e7729100882b880221f613de2f5b
jjribeiro1 <jvsr.99@hotmail.com>
2025-02-10 20:01:07 -0300

    add Comment type definition

---
+++ src/types/comment.ts
@@ -0,0 +1,10 @@
+export type Comment = {
+  id: string;
+  content: string;
+  authorId: string;
+  authorName: string
+  postId: string;
+  createdAt: Date;
+  updatedAt: Date;
+  deletedAt: Date | null;
+};
