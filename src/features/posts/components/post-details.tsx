9f8a8a30b19ef5f1cee21f889679adb8403b58c7
jjribeiro1 <jvsr.99@hotmail.com>
2025-02-10 19:26:24 -0300

    add post comments

--- src/features/posts/components/post-details.tsx
+++ src/features/posts/components/post-details.tsx
@@ -1,28 +1,40 @@
 "use client";
-import { DialogContent, DialogFooter, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+import { DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from "@/components/ui/dialog";
 import { usePostInfo } from "../hooks/use-post-info";
+import { usePostComments } from "../hooks/use-post-comments";
 
 type Props = {
   postId: string;
 };
 
 export function PostDetails(props: Props) {
-  const { data, isPending, error } = usePostInfo(props.postId);
+  const { data: post, isPending: postIsPending, error: postError } = usePostInfo(props.postId);
+  const { data: comments, isPending: commentsIsPending, error: commentsError } = usePostComments(props.postId);
 
-  if (isPending) {
+  if (postIsPending || commentsIsPending) {
     return <div>carregando informações do post</div>;
   }
 
-  if (error) {
+  if (postError || commentsError) {
     return <div>erro ao buscar dados</div>;
   }
 
   return (
-    <DialogContent>
+    <DialogContent className="max-w-5xl w-[1024px]">
       <DialogHeader>
-        <DialogTitle>{data.title}</DialogTitle>
+        <DialogTitle>{post.title}</DialogTitle>
+        <DialogDescription>{post.description}</DialogDescription>
       </DialogHeader>
-      <div>{data.description}</div>
+      <div>
+        <p className="font-medium leading-6 underline underline-offset-8 decoration-1 decoration-gray-200">
+          {`Comentários (${comments.length})`}
+        </p>
+        <div>
+          {comments.map((comment) => (
+            <div key={comment.id}>{comment.content}</div>
+          ))}
+        </div>
+      </div>
       <DialogFooter>Footer</DialogFooter>
     </DialogContent>
   );
---
+++ src/features/posts/hooks/use-post-comments.ts
@@ -0,0 +1,17 @@
+import { apiClient } from "@/lib/axios";
+import { Comment } from "@/types/comment";
+import { useQuery } from "@tanstack/react-query";
+
+type PostCommentsResponse = {
+  data: Comment[];
+};
+
+export function usePostComments(postId: string) {
+  return useQuery({
+    queryKey: ["post-comments", postId],
+    queryFn: async () => {
+      const res = await apiClient.get<PostCommentsResponse>(`/posts/${postId}/comments`);
+      return res.data.data;
+    },
+  });
+}
