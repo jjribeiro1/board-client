bb87527397de6857956d8b69ce63b2c73d371cbb
jjribeiro1 <jvsr.99@hotmail.com>
2025-03-04 16:11:01 -0300

    add organization link and tooltip to sidebar header

--- src/components/app-sidebar/header.tsx
+++ src/components/app-sidebar/header.tsx
@@ -1,5 +1,9 @@
 "use client";
+import Link from "next/link";
+import { CircleArrowOutUpRight } from "lucide-react";
 import { SidebarHeader as SidebarHeaderUI } from "@/components/ui/sidebar";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
+import { buttonVariants } from "@/components/ui/button";
 import { useOrganizationInfo } from "@/features/organizations/hooks/use-organization-info";
 
 type Props = {
@@ -10,7 +14,19 @@ export function SidebarHeader(props: Props) {
   const { data } = useOrganizationInfo(props.organizationId);
   return (
     <SidebarHeaderUI>
-      <p className="text-xl font-semibold tracking-tight text-center">{data?.name}</p>
+      <div className="flex items-center justify-between px-2">
+        <p className="text-xl font-semibold tracking-tight text-center">{data?.name}</p>
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Link href={`/organization/${data?.id}`} className={buttonVariants({ size: "icon", variant: "ghost" })}>
+                <CircleArrowOutUpRight className="w-3.5 h-3.5" />
+              </Link>
+            </TooltipTrigger>
+            <TooltipContent>Veja a página da sua organização</TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+      </div>
     </SidebarHeaderUI>
   );
 }
