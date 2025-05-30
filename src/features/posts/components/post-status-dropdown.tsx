89577233238b15dd2a599f1fb5d88ba0d5e6c444
jjribeiro1 <jvsr.99@hotmail.com>
2025-05-30 20:40:06 -0300

    refactor: simplify PostStatusDropdown by removing unused status colors and filtering logic

--- src/features/posts/components/post-status-dropdown.tsx
+++ src/features/posts/components/post-status-dropdown.tsx
@@ -5,18 +5,9 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Button } from "@/components/ui/button";
-import { useOrganizationStatus } from "@/hooks/use-organization-status";
 import { useUpdatePostStatusMutation } from "@/features/posts/mutations/use-update-post-status-mutation";
+import { useOrganizationStatus } from "@/hooks/use-organization-status";
 import { Post } from "@/types/post";
-
-const statusColors: { [key in number]: string } = {
-  1: "bg-primary-foreground text-white hover:bg-primary-foreground/85",
-  2: "bg-purple-500 text-white hover:bg-purple-500/85",
-  3: "bg-yellow-500 text-white hover:bg-yellow-500/85",
-  4: "bg-blue-500 text-white hover:bg-blue-500/85",
-  5: "bg-green-500 text-white hover:bg-green-500/85",
-  6: "bg-red-500 text-white hover:bg-red-500/85",
-};
 
 type Props = {
   orgId: string;
@@ -27,14 +18,6 @@ export function PostStatusDropdown(props: Props) {
   const { data } = useOrganizationStatus();
   const { mutate } = useUpdatePostStatusMutation(props.post.id, props.orgId);
 
-  function filterStatus(status: { id: string; name: string; order: number | null }) {
-    if (status.order === null || status.order <= 1 || status.name === props.post.status.name) {
-      return false;
-    }
-
-    return true;
-  }
-
   function handlePostStatusChange(statusId: string) {
     mutate({ statusId });
   }
@@ -42,12 +25,12 @@ export function PostStatusDropdown(props: Props) {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button size={"sm"} className={`${statusColors[props.post.status.order!]} h-max py-1`}>
+        <Button size={"sm"} className={`h-max py-1`}>
           {props.post.status.name}
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent>
-        {data?.filter(filterStatus).map((status) => (
+        {data?.map((status) => (
           <DropdownMenuItem key={status.id} onClick={() => handlePostStatusChange(status.id)}>
             {status.name}
           </DropdownMenuItem>
