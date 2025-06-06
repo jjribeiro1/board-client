e202fb27ae5cf4a633b1d4444285882b32065f85
jjribeiro1 <jvsr.99@hotmail.com>
2025-06-06 20:27:56 -0300

    feat: replace button with ActionAlert for comment deletion confirmation

--- src/features/comments/components/delete-comment.tsx
+++ src/features/comments/components/delete-comment.tsx
@@ -1,5 +1,6 @@
 import { Trash } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { ActionAlert } from "@/components/ui/alert";
 import { useDeleteCommentMutation } from "../mutations/use-delete-comment-mutation";
 
 type Props = {
@@ -7,15 +8,20 @@ type Props = {
 };
 
 export function DeleteComment(props: Props) {
-  const { mutate } = useDeleteCommentMutation(props.commentId);
-
-  function onClick() {
-    mutate();
-  }
+  const { mutate: deleteCommentMutation } = useDeleteCommentMutation(props.commentId);
 
   return (
-    <Button onClick={onClick} className="h-6 w-6" variant={"ghost"} size={"icon"}>
-      <Trash className="h-3.5 w-3.5" />
-    </Button>
+    <ActionAlert
+      title="Você tem certeza?"
+      description="O comentário será removido e essa ação não pode ser desfeita"
+      cancelText="Cancelar"
+      actionText="Confirmar"
+      trigger={
+        <Button className="h-6 w-6" variant={"ghost"} size={"icon"}>
+          <Trash className="h-3.5 w-3.5" />
+        </Button>
+      }
+      onAction={deleteCommentMutation}
+    />
   );
 }
