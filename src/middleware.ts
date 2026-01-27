127ac1b34450c43c799cfc19e3b90e93d84a121a
João Vitor <jvsr.99@hotmail.com>
2026-01-27 16:55:26 -0300

    feat: add '/invite' to public routes in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { refreshToken, verifyAccessToken } from "./lib/auth";
 
-const publicRoutes = ["/login", "/register"];
+const publicRoutes = ["/login", "/register", '/invite'];
 
 export default async function middleware(req: NextRequest) {
   const path = req.nextUrl.pathname;
