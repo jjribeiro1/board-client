ed7a163ed166bac115ba0f68e2c6e851663667b2
João Vitor <jvsr.99@hotmail.com>
2026-01-30 19:22:38 -0300

    feat: restructure PublicOrganizationPage layout for improved organization and clarity

--- src/app/(organizations)/organization/[id]/page.tsx
+++ src/app/(organizations)/organization/[id]/page.tsx
@@ -4,6 +4,7 @@ import { useBoardInfo } from "@/features/board/hooks/use-board-info";
 import { useQueryParams } from "@/hooks/use-query-params";
 import { useBoardPosts } from "@/features/board/hooks/use-board-posts";
 import { BoardPostCard } from "@/features/board/components/board/board-post-card";
+import { CreatePost } from "@/features/posts/components/create-post";
 
 export default function PublicOrganizationPage() {
   const { getQueryParam } = useQueryParams();
@@ -23,9 +24,15 @@ export default function PublicOrganizationPage() {
 
   return (
     <section className="flex flex-col gap-y-10 px-4">
-      <div className="flex flex-col gap-2">
-        <h1 className="text-3xl font-bold">{boardData.title}</h1>
-        <p className="text-secondary-foreground text-lg">{boardData.description}</p>
+      <div className="flex w-full items-center justify-between">
+        <div className="flex flex-col gap-2">
+          <h1 className="text-3xl font-bold">{boardData.title}</h1>
+          <p className="text-secondary-foreground text-lg">{boardData.description}</p>
+        </div>
+
+        <div className="pr-6">
+          <CreatePost organizationId={boardData.organizationId} />
+        </div>
       </div>
 
       <div className="flex flex-col gap-y-8">
